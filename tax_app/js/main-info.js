f41a0687f79ed62b22be95c568a226895f86e377
Irina Levina <irina.levina.i@gmail.com>
2015-10-09 08:48:29 +0300

    minify files

--- tax_app/js/main-info.js
+++ tax_app/js/main-info.js
@@ -1,15 +1,1 @@
-$(document).ready(function(){
-	$('#anotherCity').select2();
-
-	addInfoPageEvents();
-});
-
-
-
-var addInfoPageEvents = function () {
-	$('.filter-swither-item').on("click", function () {
-		$(this).parents('.filter-switcher').find('.filter-swither-item').removeClass('_active');
-		$(this).addClass('_active');
-	});
-}
-
+$(document).ready(function(){$("#anotherCity").select2(),addInfoPageEvents()});var addInfoPageEvents=function(){$(".filter-swither-item").on("click",function(){$(this).parents(".filter-switcher").find(".filter-swither-item").removeClass("_active"),$(this).addClass("_active")})};
